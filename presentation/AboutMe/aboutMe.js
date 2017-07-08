0670476f074f80d758f8c98b95e36f88496bca01
Richard Young <richard@bnry.local>
2017-07-08 12:30:08 +0200

    About me slide

--- presentation/AboutMe/aboutMe.js
+++ presentation/AboutMe/aboutMe.js
@@ -9,13 +9,21 @@ import {
     Quote,
     Slide,
     Text,
+    Image
 } from "spectacle";
+
+import bnryLogo from '../../assets/bnry.png';
+import twitter from '../../assets/Twitter.png';
+import github from '../../assets/github.png';
+import './style.css'
 
 export function aboutMeSlide() {
     return (
-        <Slide transition={["fade"]} notes={title_notes}>
-            <Heading size={5} textColor="header">Richard Young</Heading>
-            <Heading size={5} textColor="header">BNRY Digital</Heading>
+        <Slide transition={["fade"]} notes={title_notes} className='aboutMe'>
+            <Heading size={3} textColor="header">Richard Young</Heading>
+            <Heading size={5} textColor="secondary" className='subheader'><Image src={twitter} className='contactLogo'/>@richardyoung00</Heading>
+            <Heading size={5} textColor="secondary" className='subheader'><Image src={github} className='contactLogo'/>github.com/richardyoung00</Heading>
+            <Image src={bnryLogo} className='logo'/>
         </Slide>
     );
 }
